f988e2cf8275d477d7b6d02f60dbbcb30e363e3d
MEFCO <151456428+themefco@users.noreply.github.com>
2026-09-20 15:14:38 +0300

    Create sw2.js

---
+++ sw2.js
@@ -0,0 +1,11 @@
+const CACHE='mefco-watch-shell-v1';
+const APP=['./','./index.html','./manifest.json'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP).catch(()=>{})).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+ const req=event.request;
+ if(req.method!=='GET')return;
+ const url=new URL(req.url);
+ if(url.origin!==location.origin)return;
+ event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{if(res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{})}return res}).catch(()=>caches.match('./index.html'))));
+});
